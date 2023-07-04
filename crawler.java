61009fe2991b34fe1744cafd4ac069bf39c52138
rkarmaka98 <rkarmaka98@gmail.com>
2023-07-04 22:19:05 +0530

    Adding comment

--- crawler.java
+++ crawler.java
@@ -6,11 +6,14 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class crawler {
+    //URL of the targeted website to crawl from
     public static String url="https://en.wikipedia.org/wiki/Java_(programming_language)";
 
     public static void main(String[] args) throws IOException {
+        //Setup HTTP connection with the server and use get() method for GET request to get DOM tree
         Connection conn = Jsoup.connect(url);
         Document doc = conn.get();
+        //Now parse the document by element id and paras
         Element content=doc.getElementById("mw-content-text");
         Elements paragraphs=content.select("p");
     }
